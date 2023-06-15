4fcbe9488e2f04e7b48125b5bc75a378ed22e68c
kadirgurturk <89667179+kadirgurturk@users.noreply.github.com>
2023-06-15 15:08:03 +0300

    pageable done

--- src/component/downcontainer/Pageable.jsx
+++ src/component/downcontainer/Pageable.jsx
@@ -4,21 +4,71 @@ import arrow from "../../asset/downcontainer/arrow.svg"
 let totalpage = 234 / 6;
 
 export default function () {
-    const [currpage,setCurrPage] = useState(1);
-    
-    
+  const [inputVal,setInpuVal] = useState("")
+  const [currpage, setCurrPage] = useState(inputVal);
+  const [rightArrow,setRightArrow] = useState(true);
+  const [leftArrow,setLefttArrow] = useState(true);
+  const totalPage = 6;
 
-    let totalPage = 25;
+  const handleCurrPageChange = (event) => {
+    const value = event.target.value;
+    const parsedValue = parseInt(value, 10);
+  
+    if (parsedValue > totalPage || parsedValue < 1 || isNaN(parsedValue)) {
+      alert(`Lütfen 1 ile ${totalPage} arası bir sayı giriniz`);
 
+      return;
+    }
+    setInpuVal(parsedValue);
+  };
+
+  useEffect(() => {
+    setInpuVal(1);
+  }, []);
+
+  useEffect(() => {
+    setCurrPage(inputVal);
+  }, [inputVal]);
+
+  
+  const leftClick = () =>{
+    if(currpage > 1){
+      let newPage = currpage - 1;
+      setInpuVal(newPage)
+    }
+  }
+
+  const rightClick = () =>{
+    if(currpage < totalPage){
+      let newPage = currpage + 1;
+      setInpuVal(newPage)
+    }
+  }
+
+  useEffect(() => {
+    if(inputVal === totalPage){
+      setRightArrow(false)
+    }else{
+      setRightArrow(true)
+    }
+
+    if(inputVal === 1){
+      setLefttArrow(false)
+    }
+    else{
+      setLefttArrow(true)
+    }
+
+}, [currpage,inputVal]);
 
 
   return (
     <div className='pagaeble'>
-        <img className='left' src={arrow} alt="" />
-        <input id='currpage'  type="number" min="1" max={totalPage} />
+        <img onClick={leftClick} className={leftArrow ? "left" : "disableLeft"} src={arrow} alt="" />
+        <input id='currpage'  type="number" value={inputVal}  onChange={handleCurrPageChange} />
         <span id='text'>of</span>
         <span id='totalpage'>6</span>
-        <img className='right' src={arrow} alt="" />
+        <img onClick={rightClick} className={rightArrow ? "right" : "disableRight"} src={arrow} alt="" />
     </div>
   )
 }

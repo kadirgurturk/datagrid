c23f1a27f8717d40b50f2c24b589228dd544c1a1
kadirgurturk <89667179+kadirgurturk@users.noreply.github.com>
2023-06-24 03:41:37 +0300

    mobile design done

--- src/component/header/MobileHeader.jsx
+++ src/component/header/MobileHeader.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import hamburger from "../../asset/header/hamburger.svg"
+import logo from "../../asset/header/logo.svg"
+import instagram from "../../asset/header/instagram.svg"
+import be from "../../asset/header/be.svg"
+import youtube from "../../asset/header/youtube.svg"
+import linkedin from "../../asset/header/linkedin.svg"
 
 export default function MobileHeader() {
-  return (
-    <div className='mobileheader'>
+    const [isOpen,setOpen] = useState(false)
 
-    </div>
+  return (
+    <nav className='mobileheader'>
+      {
+        isOpen ? (
+            <nav className='sidebar'>
+            <img onClick={()=>{setOpen(false)}} id="hamburgerclose" src={hamburger} alt="hamburger" />
+            <li className='site_links'>
+              <ul>
+                <a href=""><span>Hakkımızda</span></a>
+                <a href=""><span>Jüri - Yarışma Yazılımı</span></a>
+                <a href=""><span>Word Ninja</span></a>
+                <a href=""><span>Word Pyramids</span></a>
+              </ul>
+            </li>
+            <li className='social_links'>
+              <img src={youtube} alt="" />
+              <img src={instagram} alt="" />
+              <img src={be} alt="" />
+              <img src={linkedin} alt="" />
+            </li>
+      </nav>
+            
+          ) : (
+            <div className='mobileheader'>
+                <img onClick={()=>{setOpen(true)}} id="hamburger" src={hamburger} alt="hamburger" />
+                <img id="logo" src={logo} alt="logo" />
+            </div>
+            )
+      }
+
+    </nav>
   )
 }
